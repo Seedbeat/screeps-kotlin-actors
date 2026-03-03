ba89150963f4d497a4c6456e169eed7e3b809ccf
Seedbeat <tonchegru@gmail.com>
2026-03-04 01:12:59 +0300

    Make base RoomActor initializing from SystemActor Bootstrap

--- src/jsMain/kotlin/actors/SystemActor.kt
+++ src/jsMain/kotlin/actors/SystemActor.kt
@@ -3,8 +3,11 @@ package actors
 import actor.Actor
 import actor.ActorSystem
 import actor.message.IMessage
+import actor.message.IPayload
+import actors.base.Lifecycle
 import screeps.api.Game
 import screeps.api.values
+import screeps.utils.lazyPerTick
 import utils.log.ILogging
 import utils.log.LogLevel
 import utils.log.Logging
@@ -41,71 +44,44 @@ class SystemActor(id: String) : Actor(id), ILogging by Logging<SystemActor>(LogL
         }
     }
 
-    private suspend fun onLifecycle(msg: Lifecycle) = when (msg) {
-        is Lifecycle.Bootstrap -> onBootstrap()
-        is Lifecycle.Tick -> onTick(msg.time)
+    private val ownedRoomsNames by lazyPerTick {
+        Game.rooms.values.filter { room -> room.controller?.my == true }.map { room -> room.name }.toSet()
     }
 
-    private suspend fun onTick(time: Int) {
-        log.info("System tick: $time")
+    private suspend fun onLifecycle(msg: Lifecycle) = when (msg) {
+        is Lifecycle.Bootstrap -> onBootstrap()
+        is Lifecycle.Tick -> onTick(msg)
+    }
 
-        // Тут позже будет:
-        // 1. sync room actors
-        // 2. send RoomCommand.OnTick(...)
+    private suspend fun onTick(msg: Lifecycle.Tick) {
+        log.info("System tick: ${msg.time}")
+
+        syncGameObjectsWithActors()
+        broadcast(msg)
     }
 
     private suspend fun onBootstrap() {
         log.info("System bootstrap")
 
-        // Тут позже можно:
-        // 1. создать стартовые акторы
-        // 2. прогреть состояние
-
         syncGameObjectsWithActors()
-        // TODO: sendTickMessages(setOf())
+        broadcast(Lifecycle.Bootstrap)
     }
 
     fun syncGameObjectsWithActors() {
-        val activeActors = mutableSetOf<String>()
 
-        Game.rooms.values
-            .filter { room -> room.controller?.my == true }
-            .forEach { room ->
-                activeActors.add(room.name)
+        val roomActorsIds = ActorSystem.actors().values
+            .filterIsInstance<RoomActor>()
+            .map { it.id }
+            .toSet()
 
-                if (!ActorSystem.contains(room.name)) {
-                    ActorSystem.spawn(actorId = room.name, create = ::RoomActor)
-                }
-            }
+        val toCreate = ownedRoomsNames - roomActorsIds
+        val toRemove = roomActorsIds - ownedRoomsNames
 
-        Game.spawns.values
-            .filter { spawn -> spawn.my }
-            .forEach { spawn ->
-                activeActors.add(spawn.id)
-
-                if (!ActorSystem.contains(spawn.id)) {
-                    ActorSystem.spawn(actorId = spawn.id, create = ::SpawnActor)
-                }
-            }
-
-        removeStaleActors(activeActors)
+        toCreate.forEach { actorId -> ActorSystem.spawn(actorId, ::RoomActor) }
+        toRemove.forEach { actorId -> ActorSystem.remove(actorId) }
     }
 
-    private fun removeStaleActors(activeActors: Set<String>) {
-        ActorSystem.actors().keys.forEach { actorId ->
-
-            val isStale = !activeActors.contains(actorId)
-
-            if (isStale) {
-                ActorSystem.remove(actorId)
-                log.info("Removed stale actor: $actorId")
-            }
-        }
-    }
-
-    private fun sendTickMessages(actorsIds: Set<String>) {
-        actorsIds.forEach { actorId ->
-            sendTo(actorId, RoomCommand.OnTick(Game.time))
-        }
+    private fun broadcast(payload: IPayload) {
+        ownedRoomsNames.forEach { actorId -> sendTo(actorId, payload) }
     }
 }
