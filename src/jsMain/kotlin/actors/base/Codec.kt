342de79f80989165217f6e301d52ce58e536b5fd
Seedbeat <tonchegru@gmail.com>
2026-03-26 13:31:00 +0300

    Add nullable codec helper

--- src/jsMain/kotlin/actors/base/Codec.kt
+++ src/jsMain/kotlin/actors/base/Codec.kt
@@ -4,3 +4,11 @@ interface Codec<T> {
     fun serialize(data: T): dynamic
     fun deserialize(raw: dynamic): T
 }
+
+fun <T> Codec<T>.asNullable(): Codec<T?> = object : Codec<T?> {
+    override fun serialize(data: T?): dynamic =
+        if (data == null) null else this@asNullable.serialize(data)
+
+    override fun deserialize(raw: dynamic): T? =
+        if (raw == null) null else this@asNullable.deserialize(raw)
+}
