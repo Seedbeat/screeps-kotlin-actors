f717887a53345cf7bde9da0a9e8209d211c3c6e7
Seedbeat <tonchegru@gmail.com>
2026-03-30 15:18:43 +0300

    Simplify CreepActor

--- src/jsMain/kotlin/actors/CreepRequest.kt
+++ src/jsMain/kotlin/actors/CreepRequest.kt
@@ -3,6 +3,5 @@ package actors
 import actor.message.Request
 
 sealed class CreepRequest<T> : Request<T> {
-    data object StatusRequest : CreepRequest<CreepStatus>()
-    data object Unassign : CreepRequest<Boolean>()
+    data object Status : CreepRequest<CreepStatus>()
 }
--- src/jsMain/kotlin/actors/CreepResponse.kt
+++ src/jsMain/kotlin/actors/CreepResponse.kt
@@ -3,6 +3,5 @@ package actors
 import actor.message.Response
 
 sealed class CreepResponse<T> : Response<T> {
-    data class StatusResponse(override val result: CreepStatus) : CreepResponse<CreepStatus>()
-    data class UnassignResponse(override val result: Boolean) : CreepResponse<Boolean>()
+    data class Status(override val result: CreepStatus) : CreepResponse<CreepStatus>()
 }
